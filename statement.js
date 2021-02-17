8f8fa5726686cb0e872db8420d99407f16b2462d
takechiyo-19940627 <takehiro.tanaka@playground.live>
2021-02-17 09:56:41 +0900

    refactor:fetch play from function

--- statement.js
+++ statement.js
@@ -32,7 +32,7 @@ function statement (invoice, plays) {
   const format = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFranctionDigits: 2}).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
     // ボリューム特典のポイントを加算
     volumeCredits += Math.max(perf .audience - 30, 0);
@@ -66,5 +66,9 @@ function statement (invoice, plays) {
         throw new Error(`unknown type: ${play.type}`);
     }
     return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
 }
