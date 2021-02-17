b3bff4c96739e6e33a6c4401bfca302b220aeb43
takechiyo-19940627 <takehiro.tanaka@playground.live>
2021-02-17 09:46:47 +0900

    Cadd first code

---
+++ statement.js
@@ -0,0 +1,67 @@
+// NOTE: 必要なデータ
+var plays = {
+  "hamlet": {"name": "Hamlet", "type": "tragedy"},
+  "as-like": {"name": "As You Like It", "type": "comedy"},
+  "othello": {"name": "Othelo", "type": "tragedy"}
+};
+var invoices = [
+  {
+    "customer": "BigCo",
+    "performances": [
+      {
+        "playID": "hamlet",
+        "audience": 55
+      },
+      {
+        "playID": "as-like",
+        "audience": 35
+      },
+      {
+        "playID": "othello",
+        "audience": 40
+      }
+    ]
+  }
+];
+statement (invoices[0], plays);
+
+function statement (invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+  const format = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFranctionDigits: 2}).format;
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = 0;
+
+    switch (play.type) {
+      case "tragedy":
+        thisAmount = 400000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 1000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+    // ボリューム特典のポイントを加算
+    volumeCredits += Math.max(perf .audience - 30, 0);
+    // 喜劇のときは10人につきさらにポイントを加算
+    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    // 注文の内訳を出力
+    result += `${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+    totalAmount += thisAmount;
+  }
+  result += `Amount owed is ${format(totalAmount/100)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+  console.log(result);
+  return result;
+}
