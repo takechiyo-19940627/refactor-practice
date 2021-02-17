8e23dfaa6d09fc74e648a3cef1a0682e508dfe81
takechiyo-19940627 <takehiro.tanaka@playground.live>
2021-02-17 22:18:53 +0900

    refactor:change loop function

--- statement.js
+++ statement.js
@@ -75,23 +75,15 @@ function statement (invoice, plays) {
   }
 
   function totalAmount(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 
   function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
   for (let perf of data.performances) {
     result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
