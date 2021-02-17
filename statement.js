fd60b562da0770c0e9598bba10de3e84becb544e
takechiyo-19940627 <takehiro.tanaka@playground.live>
2021-02-17 21:57:36 +0900

    refacotr:move totalAmount and volumeCredits function to statement function

--- statement.js
+++ statement.js
@@ -29,6 +29,8 @@ function statement (invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
   return renderPlainText(statementData, plays);
 
   function enrichPerformance(aPerformance) {
@@ -71,6 +73,22 @@ function statement (invoice, plays) {
     if ("comedy" === aPerformance.play.type) volumeCredits += Math.floor(aPerformance.audience / 5);
     return volumeCredits;
   }
+
+  function totalAmount(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.amount;
+    }
+    return result;
+  }
+
+  function totalVolumeCredits(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.volumeCredits;
+    }
+    return result;
+  }
 }
 
 function renderPlainText(data, plays) {
@@ -79,29 +97,13 @@ function renderPlainText(data, plays) {
     result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
   }
 
-  result += `Amount owed is ${usd(totalAmount())}\n`;
-  result += `You earned ${totalVolumeCredits()} credits\n`;
+  result += `Amount owed is ${usd(data.totalAmount)}\n`;
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US",
                 { style: "currency", currency: "USD",
                   minimumFranctionDigits: 2 }).format(aNumber/100);
-  }
-
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
-  }
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
   }
 }
