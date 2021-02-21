9f7a4de72e3d23351dca9f4ff423fa08232c91cb
takechiyo-19940627 <takehiro.tanaka@playground.live>
2021-02-21 15:05:59 +0900

    test:add noproducers test

--- test/test-province.js
+++ test/test-province.js
@@ -12,6 +12,45 @@ describe('province', function() {
   it('profit', function() {
     expect(asia.profit).equal(230);
   });
+  it('change production', function() {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).equal(-6);
+    expect(asia.profit).equal(292);
+  });
+  it('zero demand', function() {
+    asia.demand = 0;
+    expect(asia.shortfall).equal(-25);
+    expect(asia.profit).equal(0);
+  });
+  it('negative demand', function() {
+    asia.demand = -1;
+    expect(asia.shortfall).equal(-26);
+    expect(asia.profit).equal(-10);
+  });
+  it('empty string demand', function() {
+    asia.demand = "";
+    expect(asia.shortfall).NaN;
+    expect(asia.profit).NaN;
+  });
+});
+
+describe('no producers', function() {
+  let noProducers;
+  beforeEach(function() {
+    const data = {
+      name: "No producers",
+      producers: [],
+      demand: 30,
+      price: 20,
+    };
+    noProducers = new Province(data);
+  });
+  it('shortfall', function() {
+    expect(noProducers.shortfall).equal(30);
+  });
+  it('profit', function() {
+    expect(noProducers.profit).equal(0);
+  });
 });
 
 // NOTE: 以下実装のコード
